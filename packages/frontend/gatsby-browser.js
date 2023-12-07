ed678718697692f39c27d3c5c2642880c0e62b92
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2023-12-07 13:42:49 +0000

    Maps more pages

--- packages/frontend/gatsby-browser.js
+++ packages/frontend/gatsby-browser.js
@@ -5,6 +5,9 @@ import IndexPage from './src/pages/index';
 import ShopPage from './src/pages/shop';
 import OrderConfirmPage from './src/pages/orderConfirm';
 import PrivateRoute from "./src/components/PrivateRoute";
+import LoginPage from './src/pages/login';
+import ForgotPage from './src/pages/forgot';
+import HowToPage from './src/pages/how-to-use';
 
 import { NotificationProvider } from './src/context/AddItemNotificationProvider';
 
@@ -14,6 +17,9 @@ export const wrapRootElement = ({ element }) => (
         <IndexPage path='/'></IndexPage>
         <SignupPage path='/signup'></SignupPage>
         <ShopPage path='/shop'></ShopPage>
+        <LoginPage path='/login'></LoginPage>
+        <ForgotPage path='/forgot'></ForgotPage>
+        <HowToPage path='how-to-use'></HowToPage>
         <PrivateRoute path="/product/sample" component={OrderConfirmPage} />
       </Router>
       </NotificationProvider>
